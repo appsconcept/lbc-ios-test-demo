a6fb07da12d401b4cd1537ade31db93a333faee1
Marco <marco@appsconcept.eu>
2023-03-29 11:00:30 +0100

    [DependencyInjection] Implement ServiceProvider to manage the services

---
+++ LBCiOSTestDemo/Logic/Services/Provider/ServiceProvider.swift
@@ -0,0 +1,12 @@
+//
+//  ServiceProvider.swift
+//  LBCiOSTestDemo
+//
+//  Created by Marco Appsconcept on 29/03/2023.
+//
+
+import Foundation
+
+protocol ServiceProvider {
+  // TODO: Declare services after implementing them
+}
---
+++ LBCiOSTestDemo/Logic/Services/Provider/ServiceProviderImpl.swift
@@ -0,0 +1,12 @@
+//
+//  ServiceProviderImpl.swift
+//  LBCiOSTestDemo
+//
+//  Created by Marco Appsconcept on 29/03/2023.
+//
+
+import Foundation
+
+class ServiceProviderImpl: ServiceProvider {
+  // TODO: Implement init after implementing new services
+}
