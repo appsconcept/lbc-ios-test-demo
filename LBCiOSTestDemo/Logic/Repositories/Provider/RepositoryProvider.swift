d769cd0915d9219ff070d4776a35c8ce9bfd3573
Marco <marco@appsconcept.eu>
2023-03-29 11:04:31 +0100

    [DependencyInjection] Implement RepositoryProvider to manage the repositories

---
+++ LBCiOSTestDemo/Logic/Repositories/Provider/RepositoryProvider.swift
@@ -0,0 +1,12 @@
+//
+//  RepositoryProvider.swift
+//  LBCiOSTestDemo
+//
+//  Created by Marco Appsconcept on 29/03/2023.
+//
+
+import Foundation
+
+protocol RepositoryProvider {
+  // TODO: Declare repositories after implementing them
+}
---
+++ LBCiOSTestDemo/Logic/Repositories/Provider/RepositoryProviderImpl.swift
@@ -0,0 +1,12 @@
+//
+//  RepositoryProviderImpl.swift
+//  LBCiOSTestDemo
+//
+//  Created by Marco Appsconcept on 29/03/2023.
+//
+
+import Foundation
+
+class RepositoryProviderImpl: RepositoryProvider {
+  // TODO: Implement init after implementing new repositories
+}
