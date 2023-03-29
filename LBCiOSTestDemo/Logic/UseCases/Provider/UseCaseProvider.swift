22b7b056066ec41b0de96729fc465db225700729
Marco <marco@appsconcept.eu>
2023-03-29 11:06:08 +0100

    [DependencyInjection] Implement UseCaseProvider to manage the usecases

---
+++ LBCiOSTestDemo/Logic/UseCases/Provider/UseCaseProvider.swift
@@ -0,0 +1,12 @@
+//
+//  UseCaseProvider.swift
+//  LBCiOSTestDemo
+//
+//  Created by Marco Appsconcept on 29/03/2023.
+//
+
+import Foundation
+
+protocol UseCaseProvider {
+  // TODO: Declare usecases after implementing them
+}
---
+++ LBCiOSTestDemo/Logic/UseCases/Provider/UseCaseProviderImpl.swift
@@ -0,0 +1,12 @@
+//
+//  UseCaseProviderImpl.swift
+//  LBCiOSTestDemo
+//
+//  Created by Marco Appsconcept on 29/03/2023.
+//
+
+import Foundation
+
+class UseCaseProviderImpl: UseCaseProvider {
+  // TODO: Implement init after implementing new usecases
+}
